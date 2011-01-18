79b087e7af40e631d3e9e971aa9cd680dc37ca07
Alan Ott <alan@signal11.us>
2011-01-18 03:04:37 -0500

    HID: bt: Move hid_add_device() call to after hidp_session() has started.
    Move the call to hid_add_device() (which calls a device's probe() function)
    to after the kernel_thread() call which starts the hidp_session() thread.
    This ensures the Bluetooth receive socket is fully running by the time a
    device's probe() function is called. This way, a device can communicate
    (send and receive) with the Bluetooth device from its probe() function.
    
    Signed-off-by: Alan Ott <alan@signal11.us>
    Acked-by: Gustavo F. Padovan <padovan@profusion.mobi>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- net/bluetooth/hidp/core.c
+++ net/bluetooth/hidp/core.c
@@ -582,6 +582,8 @@ static int hidp_session(void *arg)
 	init_waitqueue_entry(&intr_wait, current);
 	add_wait_queue(sk_sleep(ctrl_sk), &ctrl_wait);
 	add_wait_queue(sk_sleep(intr_sk), &intr_wait);
+	session->waiting_for_startup = 0;
+	wake_up_interruptible(&session->startup_queue);
 	while (!atomic_read(&session->terminate)) {
 		set_current_state(TASK_INTERRUPTIBLE);
 
@@ -784,6 +786,8 @@ static struct hid_ll_driver hidp_hid_driver = {
 	.hidinput_input_event = hidp_hidinput_event,
 };
 
+/* This function sets up the hid device. It does not add it
+   to the HID system. That is done in hidp_add_connection(). */
 static int hidp_setup_hid(struct hidp_session *session,
 				struct hidp_connadd_req *req)
 {
@@ -825,15 +829,7 @@ static int hidp_setup_hid(struct hidp_session *session,
 
 	hid->hid_output_raw_report = hidp_output_raw_report;
 
-	err = hid_add_device(hid);
-	if (err < 0)
-		goto failed;
-
 	return 0;
-
-failed:
-	hid_destroy_device(hid);
-	session->hid = NULL;
 
 fault:
 	kfree(session->rd_data);
@@ -889,6 +885,8 @@ int hidp_add_connection(struct hidp_connadd_req *req, struct socket *ctrl_sock, 
 	skb_queue_head_init(&session->ctrl_transmit);
 	skb_queue_head_init(&session->intr_transmit);
 
+	init_waitqueue_head(&session->startup_queue);
+	session->waiting_for_startup = 1;
 	session->flags   = req->flags & (1 << HIDP_BLUETOOTH_VENDOR_ID);
 	session->idle_to = req->idle_to;
 
@@ -911,6 +909,14 @@ int hidp_add_connection(struct hidp_connadd_req *req, struct socket *ctrl_sock, 
 	err = kernel_thread(hidp_session, session, CLONE_KERNEL);
 	if (err < 0)
 		goto unlink;
+	while (session->waiting_for_startup) {
+		wait_event_interruptible(session->startup_queue,
+			!session->waiting_for_startup);
+	}
+
+	err = hid_add_device(session->hid);
+	if (err < 0)
+		goto err_add_device;
 
 	if (session->input) {
 		hidp_send_ctrl_message(session,
@@ -923,6 +929,12 @@ int hidp_add_connection(struct hidp_connadd_req *req, struct socket *ctrl_sock, 
 
 	up_write(&hidp_session_sem);
 	return 0;
+
+err_add_device:
+	hid_destroy_device(session->hid);
+	session->hid = NULL;
+	atomic_inc(&session->terminate);
+	hidp_schedule(session);
 
 unlink:
 	hidp_del_timer(session);
