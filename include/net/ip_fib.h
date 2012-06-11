ad4b24972c5d187fdacb9b6697d2839f42d3863c
David S. Miller <davem@davemloft.net>
2012-06-11 00:01:52 -0700

    inet: Add inetpeer tree roots to the FIB tables.
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/ip_fib.h
+++ include/net/ip_fib.h
@@ -20,6 +20,7 @@
 #include <linux/seq_file.h>
 #include <linux/rcupdate.h>
 #include <net/fib_rules.h>
+#include <net/inetpeer.h>
 
 struct fib_config {
 	u8			fc_dst_len;
@@ -171,11 +172,12 @@ extern __be32 fib_info_update_nh_saddr(struct net *net, struct fib_nh *nh);
 					 FIB_RES_SADDR(net, res))
 
 struct fib_table {
-	struct hlist_node tb_hlist;
-	u32		tb_id;
-	int		tb_default;
-	int		tb_num_default;
-	unsigned long	tb_data[0];
+	struct hlist_node	tb_hlist;
+	u32			tb_id;
+	int			tb_default;
+	int			tb_num_default;
+	struct inet_peer_base	tb_peers;
+	unsigned long		tb_data[0];
 };
 
 extern int fib_table_lookup(struct fib_table *tb, const struct flowi4 *flp,
