e3ff1cf2490bfc0d9be569e3d95c37006fb13ee9
Eric Dumazet <edumazet@google.com>
2012-09-25 11:32:13 +0000

    net: remove sk_init() helper
    It seems sk_init() has no value today and even does strange things :
    
    # grep . /proc/sys/net/core/?mem_*
    /proc/sys/net/core/rmem_default:212992
    /proc/sys/net/core/rmem_max:131071
    /proc/sys/net/core/wmem_default:212992
    /proc/sys/net/core/wmem_max:131071
    
    We can remove it completely.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Shan Wei <davidshan@tencent.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/socket.c
+++ net/socket.c
@@ -2577,12 +2577,6 @@ static int __init sock_init(void)
 	int err;
 
 	/*
-	 *      Initialize sock SLAB cache.
-	 */
-
-	sk_init();
-
-	/*
 	 *      Initialize skbuff SLAB cache
 	 */
 	skb_init();
