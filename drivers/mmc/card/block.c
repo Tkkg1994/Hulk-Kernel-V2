0ebe26023ed5e21d2fcc97e10c3e56de10a32a8a
Sahitya Tummala <stummala@codeaurora.org>
2013-04-10 10:15:09 +0530

    mmc: block: Fix issue with block device operation mmc_blk_ioctl
    The MMC block device ioctl path is not updated with necessary
    mmc_rpm_hold and mmc_rpm_release APIs due to which the device
    is either not getting resumed if it is already runtime suspended
    or it is getting into runtime suspend state while the ioctl
    request is still in progress. This issue would be observed only
    if MMC_CAP2_CORE_RUNTIME_PM capability is defined. Fix it by
    calling mmc_rpm_hold before start of ioctl and mmc_rpm_release
    at the end of ioctl.
    
    Change-Id: If1ccebcde4f797722989963a7c69ce1cfb6484fd
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -641,6 +641,7 @@ static int mmc_blk_ioctl_cmd(struct block_device *bdev,
 
 	mrq.cmd = &cmd;
 
+	mmc_rpm_hold(card->host, &card->dev);
 	mmc_claim_host(card->host);
 
 	if (idata->ic.is_acmd) {
@@ -686,6 +687,7 @@ static int mmc_blk_ioctl_cmd(struct block_device *bdev,
 
 cmd_rel_host:
 	mmc_release_host(card->host);
+	mmc_rpm_release(card->host, &card->dev);
 
 cmd_done:
 	mmc_blk_put(md);
@@ -766,6 +768,7 @@ static int mmc_blk_ioctl_rpmb_cmd(struct block_device *bdev,
 		goto idata_free;
 	}
 
+	mmc_rpm_hold(card->host, &card->dev);
 	mmc_claim_host(card->host);
 
 	err = mmc_blk_part_switch(card, md);
@@ -862,6 +865,7 @@ static int mmc_blk_ioctl_rpmb_cmd(struct block_device *bdev,
 
 cmd_rel_host:
 	mmc_release_host(card->host);
+	mmc_rpm_release(card->host, &card->dev);
 
 idata_free:
 	for (i = 0; i < MMC_IOC_MAX_RPMB_CMD; i++) {
