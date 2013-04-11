991dada4d230b9204398b1b9a95b8237a3c96d0f
Matt Wagantall <mattw@codeaurora.org>
2013-04-10 19:37:58 -0700

    msm: clock: Update memory retention clock flags
    Introduce CLKFAG_{NO}RETAIN_PERIPH, which will eventually be used on
    8974 to control collapse of the core memory periphery. To avoid
    confusion with CLKFAG_{NO}RETAIN, rename those existing flag to
    CLKFAG_{NO}RETAIN_MEM, to better indicate that they control retention
    of the core's memory.
    
    Change-Id: I078bcc7026095ab4b94dc57362220a5b05621bec
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

--- arch/arm/mach-msm/footswitch-8x60.c
+++ arch/arm/mach-msm/footswitch-8x60.c
@@ -217,7 +217,7 @@ static int footswitch_enable(struct regulator_dev *rdev)
 	}
 
 	/* Prevent core memory from collapsing when its clock is gated. */
-	clk_set_flags(fs->core_clk, CLKFLAG_RETAIN);
+	clk_set_flags(fs->core_clk, CLKFLAG_RETAIN_MEM);
 
 	/* Return clocks to their state before this function. */
 	restore_clocks(fs);
@@ -249,7 +249,7 @@ static int footswitch_disable(struct regulator_dev *rdev)
 		return rc;
 
 	/* Allow core memory to collapse when its clock is gated. */
-	clk_set_flags(fs->core_clk, CLKFLAG_NORETAIN);
+	clk_set_flags(fs->core_clk, CLKFLAG_NORETAIN_MEM);
 
 	/* Halt all bus ports in the power domain. */
 	if (fs->bus_port0) {
@@ -309,7 +309,7 @@ static int footswitch_disable(struct regulator_dev *rdev)
 err_port2_halt:
 	msm_bus_axi_portunhalt(fs->bus_port0);
 err:
-	clk_set_flags(fs->core_clk, CLKFLAG_RETAIN);
+	clk_set_flags(fs->core_clk, CLKFLAG_RETAIN_MEM);
 	restore_clocks(fs);
 	return rc;
 }
@@ -377,7 +377,7 @@ static int gfx2d_footswitch_enable(struct regulator_dev *rdev)
 	clk_prepare_enable(fs->core_clk);
 
 	/* Prevent core memory from collapsing when its clock is gated. */
-	clk_set_flags(fs->core_clk, CLKFLAG_RETAIN);
+	clk_set_flags(fs->core_clk, CLKFLAG_RETAIN_MEM);
 
 	/* Return clocks to their state before this function. */
 	restore_clocks(fs);
@@ -407,7 +407,7 @@ static int gfx2d_footswitch_disable(struct regulator_dev *rdev)
 		return rc;
 
 	/* Allow core memory to collapse when its clock is gated. */
-	clk_set_flags(fs->core_clk, CLKFLAG_NORETAIN);
+	clk_set_flags(fs->core_clk, CLKFLAG_NORETAIN_MEM);
 
 	/* Halt all bus ports in the power domain. */
 	if (fs->bus_port0) {
@@ -453,7 +453,7 @@ static int gfx2d_footswitch_disable(struct regulator_dev *rdev)
 	return 0;
 
 err:
-	clk_set_flags(fs->core_clk, CLKFLAG_RETAIN);
+	clk_set_flags(fs->core_clk, CLKFLAG_RETAIN_MEM);
 	restore_clocks(fs);
 	return rc;
 }
