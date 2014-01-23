4ccc5ecb1293efe4ace63d5d08ff20310e64f1c3
Yan He <yanhe@codeaurora.org>
2014-01-22 16:48:57 -0800

    msm: sps: use a new flag for disconnecting pipe
    Use a new flag to indicate if BAM client has decided to disconnect
    a pipe so that we can avoid BAM IRQ handling for a disconnected
    pipe.
    
    Change-Id: I1d1e467cf7366f0b00b32674117d381432611e26
    Signed-off-by: Yan He <yanhe@codeaurora.org>

--- drivers/platform/msm/sps/sps_bam.c
+++ drivers/platform/msm/sps/sps_bam.c
@@ -169,6 +169,7 @@ static irqreturn_t bam_isr(int irq, void *ctxt)
 	list_for_each_entry(pipe, &dev->pipes_q, list) {
 		/* Check this pipe's bit in the source mask */
 		if (BAM_PIPE_IS_ASSIGNED(pipe)
+				&& (!pipe->disconnecting)
 				&& (source & pipe->pipe_index_mask)) {
 			/* This pipe has an interrupt pending */
 			pipe_handler(dev, pipe);
@@ -585,6 +586,7 @@ static void pipe_clear(struct sps_pipe *pipe)
 	pipe->mode = -1;
 	pipe->num_descs = 0;
 	pipe->desc_size = 0;
+	pipe->disconnecting = false;
 	memset(&pipe->sys, 0, sizeof(pipe->sys));
 	INIT_LIST_HEAD(&pipe->sys.events_q);
 }
