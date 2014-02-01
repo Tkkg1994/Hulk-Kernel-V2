c440db3d4d66d0d4db8293c53c475334d4852ed4
franciscofranco <franciscofranco.1990@gmail.com>
2014-02-01 16:57:51 +0000

    bcmdhd: decrease rx wakelock time from 500ms to 250ms
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	drivers/net/wireless/bcmdhd/Makefile
    	drivers/net/wireless/bcmdhd/dhd.h

--- drivers/net/wireless/bcmdhd/dhd.h
+++ drivers/net/wireless/bcmdhd/dhd.h
@@ -508,7 +508,7 @@ inline static void MUTEX_UNLOCK_SOFTAP_SET(dhd_pub_t * dhdp)
 #define DHD_OS_WAKE_LOCK_CTRL_TIMEOUT_ENABLE(pub, val) \
 	dhd_os_wake_lock_ctrl_timeout_enable(pub, val)
 #endif /* DHD_DEBUG_WAKE_LOCK */
-#define DHD_PACKET_TIMEOUT_MS	1000
+#define DHD_PACKET_TIMEOUT_MS	250
 #define DHD_EVENT_TIMEOUT_MS	1500
 
 
