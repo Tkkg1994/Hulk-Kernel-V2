acc73a0c7b1768218826bef495b4f20be69fd783
Pranav Vashi <neobuddy89@gmail.com>
2015-02-25 02:26:59 +0530

    msm: Add MSM limiter driver
    * Adapted for Shamu.
    * A "swiss knife" that features per-cpu total
      control.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -171,6 +171,14 @@ static inline unsigned long cpufreq_scale(unsigned long old, u_int div, u_int mu
 
 #endif
 };
+#ifdef CONFIG_MSM_LIMITER
+int cpufreq_set_gov(char *target_gov, unsigned int cpu);
+char *cpufreq_get_gov(unsigned int cpu);
+int cpufreq_set_freq(unsigned int max_freq, unsigned int min_freq,
+			unsigned int cpu);
+int cpufreq_get_max(unsigned int cpu);
+int cpufreq_get_min(unsigned int cpu);
+#endif
 
 /*********************************************************************
  *                          CPUFREQ GOVERNORS                        *
