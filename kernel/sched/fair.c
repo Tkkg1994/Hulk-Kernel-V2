b3a1b153687bdacbf25790c4378a2dd2c9811397
Tkkg1994 <luca.grifo@outlook.com>
2015-03-15 22:25:35 +0100

    sched: make gentle_fair_sleepers switchable via sysfs / Stweaks

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1054,6 +1054,12 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 #endif
 }
 
+static unsigned int Lgentle_fair_sleepers = 1;
+void relay_gfs(unsigned int gfs)
+{
+	Lgentle_fair_sleepers = gfs;
+}
+
 static void
 place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 {
