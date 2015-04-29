d1efb317ef494efe8fdb626ca1b2106bb609112c
Tkkg1994 <luca.grifo@outlook.com>
2015-04-29 10:11:17 +0200

    config: enable row scheduler

--- include/linux/elevator.h
+++ include/linux/elevator.h
@@ -50,7 +50,6 @@ struct elevator_ops
 
 	elevator_dispatch_fn *elevator_dispatch_fn;
 	elevator_add_req_fn *elevator_add_req_fn;
-
 	elevator_is_urgent_fn *elevator_is_urgent_fn;
 
 	elevator_activate_req_fn *elevator_activate_req_fn;
