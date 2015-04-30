5a8c71e484f004ec44449308f6b5f0ad4632dea6
Tkkg1994 <luca.grifo@outlook.com>
2015-04-30 14:15:14 +0200

    cpufreq: add lagfree governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -476,6 +476,9 @@ extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
+extern struct cpufreq_governor cpufreq_gov_lagfree;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lagfree)
 #endif
 
 
