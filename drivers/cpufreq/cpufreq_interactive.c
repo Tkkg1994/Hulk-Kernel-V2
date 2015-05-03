1f816e068b062c5893c02dd4bf6b90647d51d549
Tkkg1994 <luca.grifo@outlook.com>
2015-05-03 18:18:14 +0200

    cpufreq: interactive: Use correct kobj when creating sysfs
    cpufreq_global_kobject is no longer initialized during cpufreq_core_init.
    Fix sysfs creation by properly requesting the kobject based on
    have_governor_per_policy().
    
    Change-Id: I5f9cff68043dad8822952bd43227d948a934a1c7
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -861,7 +861,10 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 		if (atomic_inc_return(&active_count) > 1)
 			return 0;
 
-		rc = sysfs_create_group(cpufreq_global_kobject,
+		if (!have_governor_per_policy())
+			WARN_ON(cpufreq_get_global_kobject());
+
+		rc = sysfs_create_group(get_governor_parent_kobj(policy),
 				&interactive_attr_group);
 		if (rc)
 			return rc;
@@ -894,8 +897,10 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 			return 0;
 
 		input_unregister_handler(&cpufreq_interactive_input_handler);
-		sysfs_remove_group(cpufreq_global_kobject,
+		sysfs_remove_group(get_governor_parent_kobj(policy),
 				&interactive_attr_group);
+		if (!have_governor_per_policy())
+			cpufreq_put_global_kobject();
 
 		break;
 
