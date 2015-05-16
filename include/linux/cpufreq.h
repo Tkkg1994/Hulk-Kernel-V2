f301ae72fab8eaf784879004261859f6c9058fdb
Tkkg1994 <luca.grifo@outlook.com>
2015-05-16 17:14:17 +0200

    cpufreq: add new ondemandplus, thanks to @dorimanx and @Alucard24 for their great work!

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -479,6 +479,9 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASSV2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
+extern struct cpufreq_governor cpufreq_gov_ondemandplus;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandplus)
 #endif
 
 
