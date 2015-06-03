93e29e39061f353758c4c1310421a6dc5ebace7e
Tkkg1994 <luca.grifo@outlook.com>
2015-06-03 02:13:04 +0200

    usb gadget try another stuff
    Conflicts:
    	drivers/usb/gadget/android.c

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -2273,7 +2273,7 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 	char *name;
 	char buf[256], *b;
 	int err;
-	int hid_usb_enabled = NULL;
+	bool hid_usb_enabled = false;
 
 	mutex_lock(&dev->mutex);
 
@@ -2326,9 +2326,9 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 						name);
 				if (!strcmp(name, "hid")) {
 					if (hid_usb.hid_enabled == 1)
-						hid_usb_enabled = 1;
+						hid_usb_enabled = true;
 					else
-						hid_usb_enabled = NULL;
+						hid_usb_enabled = false;
 				}
 			}
 		}
