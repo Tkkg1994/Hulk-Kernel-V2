dcb1d697dc5088a8207d0b118466d6734a41cb79
Tkkg1994 <luca.grifo@outlook.com>
2015-06-17 14:47:28 +0200

    lib: update list_debug

--- lib/list_debug.c
+++ lib/list_debug.c
@@ -11,6 +11,7 @@
 #include <linux/bug.h>
 #include <linux/kernel.h>
 #include <linux/rculist.h>
+#include <linux/bug.h>
 
 /*
  * Insert a new entry between two known consecutive entries.
@@ -34,14 +35,9 @@ void __list_add(struct list_head *new,
 	WARN(new == prev || new == next,
 	     "list_add double add: new=%p, prev=%p, next=%p.\n",
 	     new, prev, next);
-#if 0
-	BUG_ON(((prev->next != next) || (next->prev != prev)) &&
-		PANIC_CORRUPTION);
-#endif
-	if ((prev->next != next) || (next->prev != prev))
-	{
-		panic("list corruption during add");
-	}
+
+	BUG_ON((prev->next != next || next->prev != prev ||
+		 new == prev || new == next) && PANIC_CORRUPTION);
 
 	next->prev = new;
 	new->next = next;
@@ -69,10 +65,7 @@ void __list_del_entry(struct list_head *entry)
 	    WARN(next->prev != entry,
 		"list_del corruption. next->prev should be %p, "
 		"but was %p\n", entry, next->prev)) {
-#if 0
 		BUG_ON(PANIC_CORRUPTION);
-#endif
-		panic("list corruption during del");
 		return;
 	}
 
@@ -101,12 +94,10 @@ void __list_add_rcu(struct list_head *new,
 		    struct list_head *prev, struct list_head *next)
 {
 	WARN(next->prev != prev,
-		"list_add_rcu corruption. next->prev should be "
-		"prev (%p), but was %p. (next=%p).\n",
+		"list_add_rcu corruption. next->prev should be prev (%p), but was %p. (next=%p).\n",
 		prev, next->prev, next);
 	WARN(prev->next != next,
-		"list_add_rcu corruption. prev->next should be "
-		"next (%p), but was %p. (prev=%p).\n",
+		"list_add_rcu corruption. prev->next should be next (%p), but was %p. (prev=%p).\n",
 		next, prev->next, prev);
 	new->next = next;
 	new->prev = prev;
