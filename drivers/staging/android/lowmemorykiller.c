2286dab22ff5fcabe39875e32b5ec1d6a0d44885
Tkkg1994 <luca.grifo@outlook.com>
2015-07-01 14:00:43 +0200

    mm: fix stuff because of this: d6972e8232962a1f2a033a649a93f4dd31b1bf4a commit

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -205,7 +205,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 #endif
 	int other_file = global_page_state(NR_FILE_PAGES) - global_page_state(NR_SHMEM);
 #if defined(CONFIG_RUNTIME_COMPCACHE) || defined(CONFIG_ZSWAP)
-	other_file -= total_swapcache_pages;
+	other_file -= total_swapcache_pages();
 #endif /* CONFIG_RUNTIME_COMPCACHE || CONFIG_ZSWAP */
 	if (lowmem_adj_size < array_size)
 		array_size = lowmem_adj_size;
