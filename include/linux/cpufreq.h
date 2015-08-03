aa35b909b14504d85d05867189c1b4bcdce25a67
Tkkg1994 <luca.grifo@outlook.com>
2015-08-03 13:29:09 +0200

    cpufreq: add barry_allen governor v1.0, adjusted for this kernel source thanks to @javilonas for his work!

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -519,6 +519,9 @@ extern struct cpufreq_governor cpufreq_gov_bluactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_OPTIMAX)
 extern struct cpufreq_governor cpufreq_gov_optimax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_optimax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
+extern struct cpufreq_governor cpufreq_gov_barry_allen;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
 #endif
 
 
