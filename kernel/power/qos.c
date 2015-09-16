38415247ba1c534bb61a3f1d3d444d7d6497a6a7
Tkkg1994 <luca.grifo@outlook.com>
2015-09-16 23:16:52 +0200

    Revert "qos: Pass the list of cpus with affected qos to notifer"
    This reverts commit 48fe937c951fc15bab6af96b823814b2ee731e8f.

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -647,8 +647,7 @@ static int pm_qos_update_bounded_target(struct pm_qos_constraints *c, s32 value,
 	return ret;
 }
 
-static inline void pm_qos_set_value_for_cpus(struct pm_qos_constraints *c,
-		struct cpumask *cpus)
+static inline void pm_qos_set_value_for_cpus(struct pm_qos_constraints *c)
 {
 	struct pm_qos_request *req = NULL;
 	int cpu;
@@ -672,11 +671,8 @@ static inline void pm_qos_set_value_for_cpus(struct pm_qos_constraints *c,
 		}
 	}
 
-	for_each_possible_cpu(cpu) {
-		if (c->target_per_cpu[cpu] != qos_val[cpu])
-			cpumask_set_cpu(cpu, cpus);
+	for_each_possible_cpu(cpu)
 		c->target_per_cpu[cpu] = qos_val[cpu];
-	}
 }
 
 /**
@@ -696,7 +692,6 @@ int pm_qos_update_target(struct pm_qos_constraints *c,
 {
 	int prev_value, curr_value, new_value, ret;
 	struct plist_node *node = &req->node;
-	struct cpumask cpus;
 
 	mutex_lock(&pm_qos_lock);
 	prev_value = pm_qos_get_value(c);
@@ -727,9 +722,8 @@ int pm_qos_update_target(struct pm_qos_constraints *c,
 
 	if (pm_qos_enabled) {
 		curr_value = pm_qos_get_value(c);
-		cpumask_clear(&cpus);
 		pm_qos_set_value(c, curr_value);
-		pm_qos_set_value_for_cpus(c, &cpus);
+		pm_qos_set_value_for_cpus(c);
 	} else {
 		curr_value = c->default_value;
 	}
@@ -737,7 +731,7 @@ int pm_qos_update_target(struct pm_qos_constraints *c,
 	if (prev_value != curr_value) {
 		blocking_notifier_call_chain(c->notifiers,
 					     (unsigned long)curr_value,
-					     &cpus);
+					     NULL);
 		ret = 1;
 	} else {
 		ret = 0;
