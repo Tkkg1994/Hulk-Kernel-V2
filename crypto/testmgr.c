8c606dd8d2222ed12d9ca9ae6f8a6c1004590d65
Tkkg1994 <luca.grifo@outlook.com>
2015-09-18 14:50:59 +0200

    fix most compiling errors/warning that come with GCC 5.2
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -481,7 +481,7 @@ static int __test_aead(struct crypto_aead *tfm, int enc,
 
 			ret = crypto_aead_setkey(tfm, key,
 						 template[i].klen);
-			if (!ret == template[i].fail) {
+			if (ret != template[i].fail) {
 				pr_err("alg: aead%s: setkey failed on test %d for %s: flags=%x\n",
 				       d, j, algo, crypto_aead_get_flags(tfm));
 				goto out;
@@ -576,7 +576,7 @@ static int __test_aead(struct crypto_aead *tfm, int enc,
 			key = template[i].key;
 
 			ret = crypto_aead_setkey(tfm, key, template[i].klen);
-			if (!ret == template[i].fail) {
+			if (ret != template[i].fail) {
 				pr_err("alg: aead%s: setkey failed on chunk test %d for %s: flags=%x\n",
 				       d, j, algo, crypto_aead_get_flags(tfm));
 				goto out;
@@ -813,7 +813,7 @@ static int test_cipher(struct crypto_cipher *tfm, int enc,
 
 		ret = crypto_cipher_setkey(tfm, template[i].key,
 					   template[i].klen);
-		if (!ret == template[i].fail) {
+		if (ret != template[i].fail) {
 			printk(KERN_ERR "alg: cipher: setkey failed "
 			       "on test %d for %s: flags=%x\n", j,
 			       algo, crypto_cipher_get_flags(tfm));
@@ -909,7 +909,7 @@ static int test_skcipher(struct crypto_ablkcipher *tfm, int enc,
 
 			ret = crypto_ablkcipher_setkey(tfm, template[i].key,
 						       template[i].klen);
-			if (!ret == template[i].fail) {
+			if (ret != template[i].fail) {
 				printk(KERN_ERR "alg: skcipher: setkey failed "
 				       "on test %d for %s: flags=%x\n", j,
 				       algo, crypto_ablkcipher_get_flags(tfm));
@@ -973,7 +973,7 @@ static int test_skcipher(struct crypto_ablkcipher *tfm, int enc,
 
 			ret = crypto_ablkcipher_setkey(tfm, template[i].key,
 						       template[i].klen);
-			if (!ret == template[i].fail) {
+			if (ret != template[i].fail) {
 				printk(KERN_ERR "alg: skcipher: setkey failed "
 				       "on chunk test %d for %s: flags=%x\n",
 				       j, algo,
