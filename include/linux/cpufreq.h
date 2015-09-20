1a47e564aee0664b59a94fed2a5551baeeb4cddc
Tkkg1994 <luca.grifo@outlook.com>
2015-09-20 02:16:44 +0200

    cpufreq: add lazy governor, optimized for this source credits go to @Cl3Kener and @javelinanddart

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -542,6 +542,9 @@ extern struct cpufreq_governor cpufreq_gov_tripndroid;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELEMENTALX)
 extern struct cpufreq_governor cpufreq_gov_elementalx;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_elementalx)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAZY)
+extern struct cpufreq_governor cpufreq_gov_lazy;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lazy)
 #endif
 
 
