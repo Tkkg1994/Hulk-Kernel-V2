faf507a381db3a302ab86913345aed22d3f82c4c
Tkkg1994 <luca.grifo@outlook.com>
2015-09-24 18:43:29 +0200

    right values for limiter

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -173,7 +173,6 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 
 // If build is user build and enforce option is not set, selinux is always permissive
 	new_value = 0;
-
 	if (new_value != selinux_enforcing) {
 		length = task_has_security(current, SECURITY__SETENFORCE);
 		if (length)
